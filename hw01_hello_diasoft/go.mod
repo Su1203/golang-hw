7df9710b7e5d421698147b77cc7b7e31dee6003c
Su1203 <tunell@lis.ru>
2026-02-19 15:20:42 +0300

    Задание 1 исправление

--- hw01_hello_diasoft/go.mod
+++ hw01_hello_diasoft/go.mod
@@ -1,5 +1,3 @@
 module github.com/fixme_my_friend/hw01_hello_diasoft
 
 go 1.26
-
-require golang.org/x/example/hello v0.0.0-20250915201037-7f05d217867b
--- hw01_hello_diasoft/main.go
+++ hw01_hello_diasoft/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
-
-	"golang.org/x/example/hello/reverse"
 )
+
+func ReverseString(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
 
 func main() {
 	text := "Hello, DIASOFT!"
-	fmt.Println(reverse.String(text))
+	fmt.Println(ReverseString(text))
 }
