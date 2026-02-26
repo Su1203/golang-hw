755bcbf739645bf89f9931f2c51a05e866a20022
Su1203 <tunell@lis.ru>
2026-02-26 17:50:54 +0300

    Задание 2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,62 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func isBab() (string, error) {
+	return "", ErrInvalidString
+}
+
+func Unpack(s string) (string, error) {
+	runes := []rune(s)
+	var result strings.Builder
+
+	//проверка 1 символа, что не число
+	if len(runes) > 1 && unicode.IsDigit(runes[0]) {
+		return isBab()
+	}
+
+	for i := 0; i < len(runes); i++ {
+		char := runes[i]
+		isString := false
+
+		//экранирован
+		if runes[i] == '\\' {
+			if unicode.IsDigit(runes[i+1]) || runes[i+1] == '\\' {
+				char = runes[i+1]
+				isString = true
+				i++
+			} else {
+				return isBab()
+			}
+		}
+		if !unicode.IsDigit(char) || isString {
+			//следующий символ - цифра
+			if i+1 < len(runes) && unicode.IsDigit(runes[i+1]) {
+
+				//нет - это не цифра, а число
+				if i+2 < len(runes) && unicode.IsDigit(runes[i+2]) {
+					fmt.Println("runes", string(char))
+					return isBab()
+				}
+
+				count, err := strconv.Atoi(string(runes[i+1]))
+				if err == nil {
+					result.WriteString(strings.Repeat(string(char), count))
+				} else {
+					return isBab()
+				}
+			} else {
+				result.WriteString(string(char))
+			}
+		}
+	}
+
+	return result.String(), nil
 }
