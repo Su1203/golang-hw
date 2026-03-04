ba4200cbdf46ef3b6f0eb3bed93100d9f81ea4a0
Su1203 <tunell@lis.ru>
2026-03-04 12:26:01 +0300

    Задание 3

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,58 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"slices"
+	"strings"
+)
+
+type wordStat struct {
+	word string
+
+	count int
+}
+
+func Top10(s string) []string {
+	result := []string{}
+
+	words := strings.Fields(s)
+
+	// посчитаем кол-во
+
+	wordCounts := make(map[string]int)
+
+	for _, word := range words {
+		wordCounts[word]++
+	}
+
+	stats := make([]wordStat, 0, len(wordCounts))
+
+	for word, count := range wordCounts {
+		stats = append(stats, wordStat{word, count})
+	}
+
+	// сортировка
+
+	slices.SortFunc(stats, func(a, b wordStat) int {
+		if a.count != b.count {
+			return b.count - a.count
+		}
+
+		if a.word < b.word {
+			return -1
+		}
+
+		if a.word > b.word {
+			return 1
+		}
+
+		return 0
+	})
+
+	coutRes := min(10, len(stats))
+
+	for _, s := range stats[:coutRes] {
+		result = append(result, s.word)
+	}
+
+	return result
 }
