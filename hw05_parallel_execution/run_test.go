95c62e2d346e8fec2636a53327d15d2352cc4fc3
Su1203 <tunell@lis.ru>
2026-04-27 15:21:57 +0300

    Задание №5

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -38,6 +38,30 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
+	t.Run("m less or equal zero returns error immediately", func(t *testing.T) {
+		tasksCount := 10
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 5
+
+		err := Run(tasks, workersCount, 0)
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.Equal(t, int32(0), runTasksCount, "no tasks should be started when m <= 0")
+
+		err = Run(tasks, workersCount, -1)
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.Equal(t, int32(0), runTasksCount, "no tasks should be started when m <= 0")
+	})
+
 	t.Run("tasks without errors", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
