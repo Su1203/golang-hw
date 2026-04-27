f870161b85fcb6b285a0d02fe4058f00757443d9
Su1203 <tunell@lis.ru>
2026-04-27 18:35:27 +0300

    Задание 8

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,52 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) == 0 {
+		return 0
+	}
+
+	c := exec.Command(cmd[0], cmd[1:]...)
+	c.Stdin = os.Stdin
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+
+	// Let's use a map for easier manipulation
+	resEnv := make(map[string]string)
+	for _, e := range os.Environ() {
+		kv := strings.SplitN(e, "=", 2)
+		if len(kv) == 2 {
+			resEnv[kv[0]] = kv[1]
+		}
+	}
+
+	for k, v := range env {
+		if v.NeedRemove {
+			delete(resEnv, k)
+		} else {
+			resEnv[k] = v.Value
+		}
+	}
+
+	newEnv := make([]string, 0, len(resEnv))
+	for k, v := range resEnv {
+		newEnv = append(newEnv, k+"="+v)
+	}
+	c.Env = newEnv
+
+	err := c.Run()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return exitError.ExitCode()
+		}
+		return 1
+	}
+
+	return 0
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		log.Fatal("usage: go-envdir /path/to/env/dir command arg1 arg2")
+	}
+
+	dir := os.Args[1]
+	cmd := os.Args[2:]
+
+	env, err := ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exitCode := RunCmd(cmd, env)
+	os.Exit(exitCode)
 }
